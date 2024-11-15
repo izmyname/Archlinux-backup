2d2bfb228995f00db62fec914df807f5fc996086
izmyname <135810812+izmyname@users.noreply.github.com>
2024-11-15 21:41:49 +0500

    Update systemd-user-enable.sh
    remove symlinking

--- home/systemd-user-enable.sh
+++ home/systemd-user-enable.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/sh
 
-ln -sf $HOME/.config/user-dirs.dirs $HOME/.config/environment.d/15-xdg-user-dirs.conf
-
 systemctl --user enable hypridle.service hyprpolkitagent.service waybar.service foot-server.service hyprpaper.service
